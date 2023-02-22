d25635301a71e919a3a6387a6261cc4d9e803500
romppabear <ronjakarhu536@gmail.com>
2023-02-23 01:22:34 +0200

    Add page dot indicator add workout

---
+++ lib/components/common/dot_indicator.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class DotIndicator extends StatefulWidget {
+  final PageController controller;
+  final int pageCount;
+  const DotIndicator(
+      {super.key, required this.controller, required this.pageCount});
+
+  @override
+  State<DotIndicator> createState() => _DotIndicatorState();
+}
+
+class _DotIndicatorState extends State<DotIndicator> {
+  @override
+  void initState() {
+    widget.controller.addListener(() {
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> dots = [];
+
+    for (var i = 0; i < widget.pageCount; i++) {
+      dots.add(GestureDetector(
+        onTap: () => widget.controller.animateToPage(
+          i,
+          duration: const Duration(milliseconds: 200),
+          curve: Curves.easeInOut,
+        ),
+        child: Container(
+          width: 10,
+          height: 10,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: widget.controller.page == i ? Colors.blue : Colors.grey,
+          ),
+        ),
+      ));
+      dots.add(const SizedBox(
+        width: 10,
+      ));
+    }
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: dots,
+    );
+  }
+}
--- lib/screens/add_workout.dart
+++ lib/screens/add_workout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gym_diary/components/common/dot_indicator.dart';
 
 class AddWorkoutScreen extends StatefulWidget {
   const AddWorkoutScreen({super.key});
@@ -8,41 +9,55 @@ class AddWorkoutScreen extends StatefulWidget {
 }
 
 class _AddWorkoutScreenState extends State<AddWorkoutScreen> {
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    _pageController = PageController();
+    super.initState();
+  }
+
   final workoutNames = ["Push", "Pull", "Legs"];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("New Workout"),
-      ),
-      body: PageView(
-        scrollDirection: Axis.horizontal,
-        children: [
-          Center(
-            child: ListView.builder(
-              itemCount: workoutNames.length,
-              shrinkWrap: true,
-              itemBuilder: ((context, index) {
-                return Padding(
-                  padding: const EdgeInsets.fromLTRB(50, 0, 50, 15),
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.grey.shade300,
-                      padding: const EdgeInsets.fromLTRB(20, 40, 40, 20),
-                    ),
-                    child: Text(workoutNames[index]),
+        appBar: AppBar(
+          title: const Text("New Workout"),
+        ),
+        body: Stack(
+          children: [
+            PageView(
+              controller: _pageController,
+              scrollDirection: Axis.horizontal,
+              children: [
+                Center(
+                  child: ListView.builder(
+                    itemCount: workoutNames.length,
+                    shrinkWrap: true,
+                    itemBuilder: ((context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.fromLTRB(50, 0, 50, 15),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text(workoutNames[index]),
+                        ),
+                      );
+                    }),
                   ),
-                );
-              }),
+                ),
+                Text("Hello world"),
+                Text("Hello world"),
+                Text("Hello world"),
+              ],
             ),
-          ),
-          Text("Hello world"),
-          Text("Hello world"),
-          Text("Hello world"),
-        ],
-      ),
-    );
+            Positioned(
+              right: 0,
+              left: 0,
+              bottom: 30,
+              child: DotIndicator(controller: _pageController, pageCount: 3),
+            )
+          ],
+        ));
   }
 }
